8f3221893b1450e8ec73121312e11a50e112a5ca
mwsadler <45072532+mwsadler@users.noreply.github.com>
2023-09-21 12:06:05 -0400

    test cors

--- app.py
+++ app.py
@@ -1,22 +1,11 @@
 from flask import Flask
 import os
-import boto3
-dynamodb = boto3.resource('dynamodb')
-db = dynamodb.Table('blue-long-gazelleCyclicDB')
+
+from flask_cors import cross_origin
 
 app = Flask(__name__)
 
 @app.route('/')
+@cross_origin()
 def hello_world():
-    leo = db.put_item(
-        Item={
-            'pk': "partition_key",
-            'sk': "sort_key",
-            'type': "cat",
-            'color': "orange"
-        }
-    )
-
-    # get an item at key "partition_key", "sort_key" from the database
-    item = db.get_item(Key={'pk': "partition_key", 'sk': "sort_key"})
-    return item
+    return 'Hello, world markkkk!'
