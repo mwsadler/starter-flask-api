6c659330ebc85b2eda2c426a0409c22b46430bd5
mwsadler <45072532+mwsadler@users.noreply.github.com>
2023-09-21 08:07:44 -0400

    mark test 2

--- app.py
+++ app.py
@@ -1,8 +1,22 @@
 from flask import Flask
 import os
+import boto3
+dynamodb = boto3.resource('dynamodb')
+db = dynamodb.Table('blue-long-gazelleCyclicDB')
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, world markkkk!'
+    leo = db.put_item(
+        Item={
+            'pk': "partition_key",
+            'sk': "sort_key",
+            'type': "cat",
+            'color': "orange"
+        }
+    )
+
+    # get an item at key "partition_key", "sort_key" from the database
+    item = db.get_item(Key={'pk': "partition_key", 'sk': "sort_key"})
+    return item
